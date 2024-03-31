a4cc93367ef9dcdd077806521d71666f5b359333
Roberto Cajueiro <beto.cajueiro@gmail.com>
2024-03-30 21:39:58 -0300

    populando o banco tabela cliente

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,4 @@
 insert into cliente (celular, cpf, nome) values ("88762089", "00012345600", "Roberto Dantas");
+insert into cliente (celular, cpf, nome) values ("88882222", "15122244400", "Bob Silva");
+insert into cliente (celular, cpf, nome) values ("99994444", "00012345600", "Maria Silva");
+insert into cliente (celular, cpf, nome) values ("88885555", "12345678900", "Carlos Santos");
